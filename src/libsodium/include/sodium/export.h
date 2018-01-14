ed50b800793033b6dec4ae929ffc7cb291b7f8ec
Frank Denis <github@pureftpd.org>
2018-01-14 23:09:46 +0100

    Solaris Studio apparently supports __attribute__()
    Fixes #660

--- src/libsodium/include/sodium/export.h
+++ src/libsodium/include/sodium/export.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <limits.h>
 
-#if !defined(__clang__) && !defined(__GNUC__)
+#if !defined(__clang__) && !defined(__GNUC__) && !defined(__SUNPRO_C)
 # ifdef __attribute__
 #  undef __attribute__
 # endif
